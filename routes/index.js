511526aa62bc1e12c89a64d6c419616d2c14ea24
Robinson <zerodata.aolink@outlook.com>
2021-02-26 08:55:23 -0300

    se crea la base de los routes de todas las collecciones, es solo la base, falta incluir los passport y los handler junto con los controllers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const products = require('../lib/products/routes')
+const products = require('../lib/routes')
+const products = require('../lib/routes')
+const products = require('../lib/routes')
+const products = require('../lib/routes')
 
 // /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
+router.use(products)
+router.use(products)
+router.use(products)
+router.use(products)
 router.use(products)
 
 module.exports = router;
