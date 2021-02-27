9adfb496249cf8b948c16db1a91fb49445159cd5
Neisser Villa <neisser.villa@pluriza.com>
2021-02-27 02:15:24 -0500

    (FIX): validation handler error && sentri commented because have an error

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api/v1', indexRouter)
 
-app.use(SentryErrors)
+// app.use(SentryErrors)
 app.use(errorHandler)
 
 module.exports = app
