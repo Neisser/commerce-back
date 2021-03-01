2f039204a9a327c2786ed06d813b64a77c7bdefb
Robinson <zerodata.aolink@outlook.com>
2021-03-01 02:40:30 -0300

    se crea la opción de cómo plataforma poder crear cupones de descuento

--- network/utils/routes.js
+++ network/utils/routes.js
@@ -8,9 +8,9 @@ const router = express.Router();
 // router.use(routes.invoices)
 // router.use(routes.notifications)
 router.use(routes.products)
+router.use(routes.coupons)
 router.use(routes.users)
 // router.use(routes.reports)
 // router.use(routes.roles)
-// router.use(routes.users)
 
 module.exports = router
