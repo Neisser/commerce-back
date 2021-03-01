f21ef04fbd56009b5c6b3b1939eee0f368402de2
Neisser Villa <neisser.villa@pluriza.com>
2021-03-01 02:18:35 -0500

    (FEATURE): get companies from home section

--- network/utils/routes.js
+++ network/utils/routes.js
@@ -3,12 +3,12 @@ const routes = require('../../lib/routes')
 const router = express.Router();
 
 
-// router.use(routes.companies)
+router.use(routes.users)
+router.use(routes.products)
+router.use(routes.companies)
 // router.use(routes.categories)
 // router.use(routes.invoices)
 // router.use(routes.notifications)
-router.use(routes.products)
-router.use(routes.users)
 // router.use(routes.reports)
 // router.use(routes.roles)
 // router.use(routes.users)
